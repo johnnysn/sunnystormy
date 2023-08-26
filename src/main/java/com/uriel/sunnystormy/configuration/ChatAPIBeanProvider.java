46736034c5a3b980acde67fefcaf8b94782dba73
johnnysn <johnnysn@gmail.com>
2023-08-26 09:58:19 -0300

    refactor: Adds spring boot annotation processor and changes bean provider class name

--- src/main/java/com/uriel/sunnystormy/configuration/ChatAPIBeanProvider.java
+++ src/main/java/com/uriel/sunnystormy/configuration/ChatAPIBeanProvider.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class OpenAIBeanProvider {
+public class ChatAPIBeanProvider {
 
     @Value("${chat.api-key}")
     @Setter
